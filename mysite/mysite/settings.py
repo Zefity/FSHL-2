5c6d9c279b1c280ed62961bb5ba79f420d2d2804
Zefity <NIKITA.SAFONOV1972@gmail.com>
2024-02-27 19:10:44 +0300

    I can't figure out Docker

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'site',
+        'USER': 'postgres',
+        'PASSWORD': '5332Dialog',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
